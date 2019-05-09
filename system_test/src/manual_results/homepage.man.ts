e1ee00f05485067f5be2a300b9c8bc56da436572
Florian Hilbinger <florian.hilbinger@hotmail.com>
2019-05-09 21:15:46 +0200

    updated wdio-workflo to v0.2.75

--- system_test/src/manual_results/homepage.man.ts
+++ system_test/src/manual_results/homepage.man.ts
@@ -1,5 +1,5 @@
 // const homepage: Workflo.IManualTestcaseResults = {
-let homepage: Workflo.IManualTestcaseResults = {
+const homepage: Workflo.IManualTestcaseResults = {
   "5.5": {
     1: {result: true, date: '2017-08-16'},
     2: {result: false, date: '2017-08-16', comment: 'only fails in internet explorer'}
